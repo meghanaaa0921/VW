5c3f102e051bcf984e5388cba76b6d502bc6a5d7
meghanaaa0921 <157830289+meghanaaa0921@users.noreply.github.com>
2024-01-26 22:36:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showPuppyModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+function goToNext() {
+    window.location.href = "valentine.html";
+}
